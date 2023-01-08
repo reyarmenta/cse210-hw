155189741aa96d9392b61b90c0289ce1b6503a70
Reynaldo <reyarmenta1@hotmail.com>
2023-01-07 23:30:24 -0700

    Made list, to know sum, average and max

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        //do-while (userNumber != 0)
+        int userNumber =-1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (type 0 when finished): ");
+
+            string userAnswer = Console.ReadLine();
+            userNumber = int.Parse(userAnswer);
+
+            numbers.Add(userNumber);
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if(number > max)
+            {
+                max = number;
+            }
+        }
+        
+        Console.WriteLine($"The max is: {max}");
+
+        
+
+
     }
 }
