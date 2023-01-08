e1ab6da3c78d1689feb1235fb8bdf223e56bc0e8
Reynaldo <reyarmenta1@hotmail.com>
2023-01-07 22:34:15 -0700

    Guess number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+
+        //Console.Write("What is the magic number? ");
+        //Console.Write("What is your guess? ");
+
+        Random randomGenerator = new Random ();
+        int number = randomGenerator.Next(1,101);
+
+
+        int guess =0;
+
+
+        while (guess!= number){
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess){
+                Console.Write("Higher ");
+            }
+
+            else if (number < guess){
+                Console.Write("Lower ");
+            }
+            else {
+                Console.Write("You guessed it!");
+            }
+        }
+
+
+
     }
 }
