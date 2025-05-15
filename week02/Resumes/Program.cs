808d224eed1d64bd62f6bdc1d164c425af64a3e2
reyarmenta <reyarmenta1@hotmail.com>
2025-05-14 21:43:03 -0600

    Week 02 Learnign Activity Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2012;
+        job1._endYear = 2020;
+
+        //Console.WriteLine($"{job1._jobTitle} ({job1._company}) {job1._startYear}-{job1._endYear}");
+        //job1.Display();
+
+        Job job2 = new Job();    
+
+        job2._company = "Microsoft";
+        job2._jobTitle = "Designer";
+        job2._startYear = 2008;
+        job2._endYear = 2016;
+
+        //Console.WriteLine($"{job2._jobTitle} ({job2._company}) {job2._startYear}-{job2._endYear}");
+        //job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Reynaldo";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        //Console.WriteLine($"{resume._jobs[1]._jobTitle}");
+        resume.Display();
     }
 }
