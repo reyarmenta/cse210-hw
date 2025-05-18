7e0d7e8bd3a5a9dfef6d17c2963ba0ca814277c9
reyarmenta <reyarmenta1@hotmail.com>
2025-05-18 17:48:00 -0600

    Worked on W01 exercises 3, 4 and 5

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        /*Console.Write("What is the magic number? ");
+        int magicNumber = 0;
+        magicNumber = int.Parse(Console.ReadLine());*/
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (magicNumber != guess)
+        {
+            Console.Write("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<float> numbers = new List<float>();
+
+        float number = -1;
+        float sum = 0;
+        float largest = 0;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = float.Parse(Console.ReadLine());
+            numbers.Add(number);
+            sum += number;
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum / (numbers.Count - 1)}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
