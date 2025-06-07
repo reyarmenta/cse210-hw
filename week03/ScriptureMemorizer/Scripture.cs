24e8af8b4f818ada2ae4bea64e7e8955464b30e5
reyarmenta <reyarmenta1@hotmail.com>
2025-06-07 17:28:05 -0600

    Created Classes for the Scripture Memorizer Program

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,25 @@
+public class Scripture
+{
+    private string _reference = Reference();
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference, string text)
+    {
+        
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDispaly()
+    {
+
+    }
+
+    public bool IsCompletelyHidden()
+    {
+
+    }
+}
